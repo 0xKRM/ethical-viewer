f3699f507f942c41a8b288466f55801208b19259
krm <0xkrm@protonmail.com>
2022-06-22 09:25:16 -0400

    Popup and autoplay works

--- source/content.js
+++ source/content.js
@@ -1,15 +1,196 @@
-import optionsStorage from './options-storage.js';
+import optionsStorage from "./options-storage.js";
 
-async function init() {
-	const options = await optionsStorage.getAll();
-	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
-	const text = options.text;
-	const notice = document.createElement('div');
-	notice.innerHTML = text;
-	document.body.append(notice);
-	notice.id = 'text-notice';
-	notice.style.border = '2px solid ' + color;
-	notice.style.color = color;
+function makePopunder(pUrl) {
+  var _parent =
+    top != self && typeof top["document"]["location"].toString() === "string"
+      ? top
+      : self;
+  var mypopunder = null;
+  var pName = Math["floor"](Math["random"]() * 1000 + 1);
+  var pWidth = window["innerWidth"];
+  var pHeight = window["innerHeight"];
+  var pPosX = window["screenX"];
+  var pPosY = window["screenY"];
+  var pWait = 3600;
+  pWait = pWait * 1000;
+  var pCap = 50000;
+  var todayPops = 0;
+  var cookie = "_.mypopunder";
+  var browser = (function () {
+    var n = navigator["userAgent"]["toLowerCase"]();
+    var b = {
+      webkit: /webkit/["test"](n),
+      mozilla: /mozilla/["test"](n) && !/(compatible|webkit)/["test"](n),
+      chrome: /chrome/["test"](n),
+      msie: /msie/["test"](n) && !/opera/["test"](n),
+      firefox: /firefox/["test"](n),
+      safari: /safari/["test"](n) && !/chrome/["test"](n),
+      opera: /opera/["test"](n),
+    };
+    b["version"] = b["safari"]
+      ? (n["match"](/.+(?:ri)[\/: ]([\d.]+)/) || [])[1]
+      : (n["match"](/.+(?:ox|me|ra|ie)[\/: ]([\d.]+)/) || [])[1];
+    return b;
+  })();
+
+  function isCapped() {
+    try {
+      todayPops = Math["floor"](
+        document["cookie"]["split"](cookie + "Cap=")[1]["split"](";")[0]
+      );
+    } catch (err) {}
+    return (
+      pCap <= todayPops || document["cookie"]["indexOf"](cookie + "=") !== -1
+    );
+  }
+
+  function doPopunder(pUrl, pName, pWidth, pHeight, pPosX, pPosY) {
+    if (isCapped()) {
+      return;
+    }
+    var sOptions =
+      "toolbar=no,scrollbars=yes,location=yes,statusbar=yes,menubar=no,resizable=1,width=" +
+      pWidth.toString() +
+      ",height=" +
+      pHeight.toString() +
+      ",screenX=" +
+      pPosX +
+      ",screenY=" +
+      pPosY;
+    document["onclick"] = function (e) {
+      if (
+        isCapped() ||
+        window["pop_clicked"] == 1 ||
+        pop_isRightButtonClicked(e)
+      ) {
+        //return;
+      }
+      window["pop_clicked"] = 1;
+      mypopunder = _parent["window"]["open"](pUrl, pName, sOptions);
+      if (mypopunder) {
+        var now = new Date();
+        document["cookie"] =
+          cookie +
+          "=1;expires=" +
+          new Date(now["setTime"](now["getTime"]() + pWait))["toGMTString"]() +
+          ";path=/";
+        now = new Date();
+        document["cookie"] =
+          cookie +
+          "Cap=" +
+          (todayPops + 1) +
+          ";expires=" +
+          new Date(now["setTime"](now["getTime"]() + 84600 * 1000))[
+            "toGMTString"
+          ]() +
+          ";path=/";
+        pop2under();
+      }
+    };
+  }
+
+  function pop2under() {
+    try {
+      mypopunder["blur"]();
+      mypopunder["opener"]["window"]["focus"]();
+      window["self"]["window"]["blur"]();
+      window["focus"]();
+      if (browser["firefox"]) {
+        openCloseWindow();
+      }
+      if (browser["webkit"]) {
+        openCloseTab();
+      }
+    } catch (e) {}
+  }
+
+  function openCloseWindow() {
+    var ghost = window["open"]("about:blank");
+    ghost["focus"]();
+    ghost["close"]();
+  }
+
+  function openCloseTab() {
+    var ghost = document["createElement"]("a");
+    ghost["href"] = "about:blank";
+    ghost["target"] = "PopHelper";
+    document["getElementsByTagName"]("body")[0]["appendChild"](ghost);
+    ghost["parentNode"]["removeChild"](ghost);
+    var clk = document["createEvent"]("MouseEvents");
+    clk["initMouseEvent"](
+      "click",
+      true,
+      true,
+      window,
+      0,
+      0,
+      0,
+      0,
+      0,
+      true,
+      false,
+      false,
+      true,
+      0,
+      null
+    );
+    ghost["dispatchEvent"](clk);
+    window["open"]("about:blank", "PopHelper")["close"]();
+  }
+
+  function pop_isRightButtonClicked(e) {
+    var rightclick = false;
+    e = e || window["event"];
+    if (e["which"]) {
+      rightclick = e["which"] == 3;
+    } else {
+      if (e["button"]) {
+        rightclick = e["button"] == 2;
+      }
+    }
+    return rightclick;
+  }
+  if (isCapped()) {
+    return;
+  } else {
+    doPopunder(pUrl, pName, pWidth, pHeight, pPosX, pPosY);
+  }
 }
 
-init();
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+waitForElm("div#content > div#description > yt-formatted-string").then(
+  function () {
+    var description = document.querySelector(
+      "div#content > div#description > yt-formatted-string"
+    ).innerText;
+
+    var regex = /original video .*youtu\.be\/(.*)/i;
+    // pull out the original video's URL
+    var match = description.match(regex);
+
+    var url =
+      "https://youtube.com/embed/" + match[1] + "?autoplay=1&mute=1&rel=0";
+    var params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=0,top=0`;
+    var player = open(url, "ethical", params).blur();
+    console.log("KRM " + match[1]);
+  }
+);
